d2fa399a01975f0ee6daa91bbb55ed4c7cf84acc
taimoorh5 <taimoorh5@gmail.com>
2019-12-04 20:12:53 -0500

    updating game

---
+++ Assets/PauseUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public  void loadMenu ()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public float movementX;
+    public float speed = 100;
+    private bool isFacingRight = true;
+    public bool getExtinguisher = false;
+    public GameObject extinguisherSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFacingRight = true;
+        if (rigid == null)
+            rigid = GetComponent<Rigidbody2D>();
+        extinguisherSprite.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxis("Horizontal");
+        if (getExtinguisher==true)
+        {
+            extinguisherSprite.SetActive(true);
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        rigid.velocity = new Vector2 (movementX * speed, rigid.velocity.y);
+        if (movementX > 0 && !isFacingRight || movementX < 0 && isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0f, 180f, 0f);
+        isFacingRight = !isFacingRight;
+    }
+
+
+}
